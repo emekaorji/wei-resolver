ce1b1f3bd51a810e160cfa6957e301d0d1637def
Emeka Orji <emekapraiseo@gmail.com>
2025-02-05 20:55:06 +0100

    debug deployment 2

---
+++ scripts/upload.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+const REFRESH_TOKEN = process.env.CHROME_REFRESH_TOKEN;
+const CLIENT_ID = process.env.CHROME_CLIENT_ID;
+const CLIENT_SECRET = process.env.CHROME_CLIENT_SECRET;
+const APP_ID = process.env.CHROME_APP_ID;
+const ZIP_FILE_PATH = './extension.zip';
+
+async function getAccessToken() {
+  const response = await fetch('https://oauth2.googleapis.com/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      refresh_token: REFRESH_TOKEN,
+      grant_type: 'refresh_token',
+    }),
+  });
+
+  const data = await response.json();
+  return data.access_token;
+}
+
+async function uploadExtension(accessToken) {
+  const zipFile = fs.readFileSync(ZIP_FILE_PATH);
+
+  const response = await fetch(
+    `https://www.googleapis.com/upload/chromewebstore/v1.1/items/${APP_ID}`,
+    {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'x-goog-api-version': '2',
+        'Content-Length': zipFile.length,
+        'Content-Type': 'application/zip',
+      },
+      body: zipFile,
+    }
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+async function publishExtension(accessToken) {
+  const response = await fetch(
+    `https://www.googleapis.com/chromewebstore/v1.1/items/${APP_ID}/publish`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'x-goog-api-version': '2',
+      },
+    }
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const accessToken = await getAccessToken();
+    const uploadResponse = await uploadExtension(accessToken);
+    console.log('Upload Response:', uploadResponse);
+
+    if (uploadResponse.uploadState === 'SUCCESS') {
+      const publishResponse = await publishExtension(accessToken);
+      console.log('Publish Response:', publishResponse);
+    } else {
+      console.error('Upload failed:', uploadResponse);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+})();
